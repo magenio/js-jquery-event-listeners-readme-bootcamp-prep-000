e53ed97277b8641676e8820f7b808f0e92a91570
VICTOR Alfonzo <valfonzom@gmail.com>
2018-05-05 15:20:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 
 function pressIt(){
   $('#typing').on('keypress', function(e){
-    if (e.listener == 71) {
+    if (e.which == 71) {
       alert("¡You press the letter G!")
     }
   })
